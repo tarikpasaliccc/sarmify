c556f3a2b955827ba0d134672b5b8740aa25e6ec
tugi4 <131661077+tugi4@users.noreply.github.com>
2024-11-15 22:25:38 +0100

    sarma catcher

--- sarmify/App.js
+++ sarmify/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import CatcherGame from './sarmaCatcher/catcherGame'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CatcherGame />
       <StatusBar style="auto" />
     </View>
   );
