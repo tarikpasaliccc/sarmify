a194c6be93f519310c0da1e1235400eded467900
Betül <kulac.esrabetul@hotmail.com>
2024-11-16 10:05:43 +0100

    memory

--- sarmify/App.js
+++ sarmify/App.js
@@ -1,14 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import MemoryGame from './memory/MemoryGame'; // Importiere die MemoryGame-Komponente
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <MemoryGame /> {/* Rendern des Memory-Spiels */}
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +18,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
