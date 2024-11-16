1dee325e708a8708af91af8563e3eb4dab4aae33
Tarik Pasalic <tpa7375@students.fhv.at>
2024-11-16 10:55:07 +0100

    update

--- sarmify/App.js
+++ sarmify/App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MemoryGame from './memory/MemoryGame'; 
 import CatcherGame from './sarmaCatcher/catcherGame'; 
-import Quiz from './components/Quiz';
+import Quiz from './quiz/Quiz';
 
 
 const App = () => {
